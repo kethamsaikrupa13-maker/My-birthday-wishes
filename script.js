5772a186e1c868a7840ee3d3411b0ab8932569ea
ketham saikrupa <kethamsaikrupa13@gmail.com>
2026-03-01 09:08:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,24 +6,39 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    document.getElementById("btn1").addEventListener("click", function () {
-        scrollToSection("section2");
-    });
+    document.getElementById("btn1").onclick = () => scrollToSection("section2");
+    document.getElementById("btn2").onclick = () => scrollToSection("section3");
+    document.getElementById("btn3").onclick = () => scrollToSection("section4");
+    document.getElementById("btn4").onclick = () => scrollToSection("section5");
 
-    document.getElementById("btn2").addEventListener("click", function () {
-        scrollToSection("section3");
-    });
+    const celebrateBtn = document.getElementById("celebrateBtn");
+    const music = document.getElementById("bgMusic");
 
-    document.getElementById("btn3").addEventListener("click", function () {
-        scrollToSection("section4");
-    });
+    celebrateBtn.addEventListener("click", function () {
 
-    document.getElementById("btn4").addEventListener("click", function () {
-        scrollToSection("section5");
+        // Play Song
+        music.currentTime = 0;
+        music.play();
+
+        // Fireworks Animation
+        var duration = 5 * 1000;
+        var end = Date.now() + duration;
+
+        (function frame() {
+            confetti({
+                particleCount: 6,
+                spread: 70,
+                origin: { y: 0.6 }
+            });
+
+            if (Date.now() < end) {
+                requestAnimationFrame(frame);
+            }
+        })();
+
     });
 
 });
-
 
 
 
